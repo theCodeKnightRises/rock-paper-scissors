cd061d0fef45d76561d588e59964444f20544270
Vijay Rathod <223536528+theCodeKnightRises@users.noreply.github.com>
2026-01-11 21:47:25 +0530

    Finish the first usable version of the game
    - Add interactive data to their respective places
    - Add log to log section
    - Many other small fixes

--- main.js
+++ main.js
@@ -1,86 +1,113 @@
-// console.log('Hello World');
+const rpsBtns = document.querySelectorAll('button');
+const roundStat = document.querySelector('#round');
+const humanStat = document.querySelector('#human');
+const computerStat = document.querySelector('#computer');
+const humanSelection = document.querySelector('.humanSelection');
+const computerSelection = document.querySelector('.computerSelection');
+const heading = document.querySelector('h1');
+const headingPara = document.querySelector('.header > p');
+const log = document.querySelector('.right > p');
 
+let humanScore = 0;
+let computerScore = 0;
+let round = 0;
+
+for (const btn of rpsBtns) {
+	btn.addEventListener('click', playRound);
+}
 
 // computerChoice
 function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3);
-    // console.log(randomNumber);
+	let randomNumber = Math.floor(Math.random() * 3);
 
-    let computerChoice;
-    // computer choices are 0-rock, 1-paper, 2-scissors
-    switch (randomNumber) {
-        case 0:
-            computerChoice = "rock";
-            break;
-        case 1:
-            computerChoice = "paper";
-            break;
-        case 2:
-            computerChoice = "scissors";
-            break;
-    }
+	let computerChoice;
+	switch (randomNumber) {
+		case 0:
+			computerChoice = 'rock';
+			break;
+		case 1:
+			computerChoice = 'paper';
+			break;
+		case 2:
+			computerChoice = 'scissors';
+			break;
+	}
 
-    console.log("Computer :", computerChoice)
-    return computerChoice;
+	console.log('Computer :', computerChoice);
+	return computerChoice;
 }
 
-// getComputerChoice();
+// playRound
+function playRound(humanChoice, computerChoice) {
+	humanChoice = this.id;
+	console.log('human :', humanChoice);
+	computerChoice = getComputerChoice();
 
+	round++;
+	roundStat.textContent = round;
+	humanSelection.textContent = humanChoice;
+	computerSelection.textContent = computerChoice;
 
-// humanChoice
-function getHumanChoice() {
-    let humanChoice = prompt("What's your weapon against AI? Rock, Paper or Scissors?").toLowerCase();
-    console.log("Human :", humanChoice);
-    return humanChoice;
+	if (
+		(humanChoice == 'rock' && computerChoice == 'scissors') ||
+		(humanChoice == 'scissors' && computerChoice == 'paper') ||
+		(humanChoice == 'paper' && computerChoice == 'rock')
+	) {
+		humanScore++;
+		// console.log("You Won!");
+		humanStat.textContent = humanScore;
+		log.innerText = `
+            round: ${round}\n\n
+            your choice: ${humanChoice.toUpperCase()}\n  
+            your score: ${humanScore}\n\n 
+            computer choice: ${computerChoice.toUpperCase()}\n
+            computer score: ${computerScore}\n\n
+            YOU WON!!!\n
+        `;
+		console.log(`Your Score is: ${humanScore}`);
+		console.log(`AI's Score is: ${computerScore}`);
+	} else if (
+		(humanChoice == 'scissors' && computerChoice == 'rock') ||
+		(humanChoice == 'paper' && computerChoice == 'scissors') ||
+		(humanChoice == 'rock' && computerChoice == 'paper')
+	) {
+		computerScore++;
+		computerStat.textContent = computerScore;
+		// console.log("AI Won!, You lost the battle!");
+		log.innerText = `
+            round: ${round}\n\n
+            your choice: ${humanChoice.toUpperCase()}\n  
+            your score: ${humanScore}\n\n
+            computer choice: ${computerChoice.toUpperCase()}\n
+            computer score: ${computerScore}\n\n 
+            AI WON!!!\n
+        `;
+		console.log(`Your Score is: ${humanScore}`);
+		console.log(`AI's Score is: ${computerScore}`);
+	} else {
+		console.log("It's a tie!");
+		log.innerText = `
+            round: ${round}\n\n
+            both of you chose same\n
+            IT'S A TIE.\n\n 
+            your score: ${humanScore}\n\n
+            computer score: ${computerScore}\n\n 
+        `;
+	}
+
+	if (humanScore === 5 || computerScore === 5) {
+		for (const btn of rpsBtns) {
+			btn.removeEventListener('click', playRound);
+
+			if (humanScore > computerScore) {
+				heading.textContent = 'you won!';
+				heading.style.color = '#9ece6a';
+				headingPara.textContent = `after ${round} rounds, you won by ${humanScore - computerScore} ${humanScore - computerScore > 1 ? 'points' : 'point'}`;
+			} else if (humanScore < computerScore) {
+				heading.textContent = 'you lost!';
+				heading.style.color = '#f7768e';
+				headingPara.textContent = `after ${round} rounds, AI Won!, you lost by ${computerScore - humanScore} ${computerScore - humanScore > 1 ? 'points' : 'point'}`;
+			}
+		}
+	}
 }
-
-// getHumanChoice();
-
-function playGame() {
-
-    let humanScore = 0;
-    let computerScore = 0;
-    // playround
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice == "rock" && computerChoice == "scissors" ||
-            humanChoice == "scissors" && computerChoice == "paper" ||
-            humanChoice == "paper" && computerChoice == "rock") {
-            humanScore++;
-            // console.log("You Won!");
-            console.log(`Your Score is: ${humanScore}`);
-            console.log(`AI's Score is: ${computerScore}`)
-        } else if (humanChoice == "scissors" && computerChoice == "rock" ||
-            humanChoice == "paper" && computerChoice == "scissors" ||
-            humanChoice == "rock" && computerChoice == "paper") {
-            computerScore++;
-            // console.log("AI Won!, You lost the battle!");
-            console.log(`Your Score is: ${humanScore}`);
-            console.log(`AI's Score is: ${computerScore}`);
-        } else {
-            console.log("It's a tie!");
-        }
-    }
-
-    for (let i = 1; i <= 5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-
-        playRound(humanSelection, computerSelection);
-
-        console.log(`Round ${i} finished!`);
-        console.log("-------------------------------------");
-    }
-
-    if (humanScore > computerScore) {
-        console.log(`After 5 rounds, You won by ${humanScore} points`);
-    } else if (humanScore < computerScore) {
-        console.log(`After 5 rounds, AI Won!, You lost by ${computerScore} points`);
-    } else if (humanScore == computerScore) {
-        console.log("After 5 rounds, It's still a tie, Try again")
-    }
-}
-
-
-playGame();
-
-
